67734c113f8cc90fe7320f56d88448d3b497c1e4
Adas <adasurbonas@gmail.com>
2025-01-08 21:44:23 +0200

    init

---
+++ wwwroot/js/site.js
@@ -0,0 +1,8 @@
+﻿window.initializeResizeHandler = (dotNetHelper) => {
+    const updateDimensions = () => {
+        dotNetHelper.invokeMethodAsync('UpdateWindowDimensions', window.innerWidth, window.innerHeight);
+    };
+
+    window.addEventListener('resize', updateDimensions);
+    updateDimensions(); // Call initially
+};
